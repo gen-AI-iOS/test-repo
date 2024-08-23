77f54890a8a8d06f75ba092b53e4c5230606d23a
矢部竜季 <aiful0703yabe@gmail.com>
2024-08-23 14:18:57 +0900

    入れ替え

---
+++ test-repo/test-repo/CustomTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTableViewCell.swift
+//  COPILOT
+//
+//  Created by YabeTatuki on 2024/08/21.
+//
+
+import UIKit
+
+class CustomTableViewCell: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
---
+++ test-repo/test-repo/DeviceInfoCell.swift
@@ -0,0 +1,26 @@
+//
+//  DeviceInfoCell.swift
+//  COPILOT
+//
+//  Created by YabeTatuki on 2024/08/22.
+//
+
+import UIKit
+
+class DeviceInfoCell: UITableViewCell {
+
+    @IBOutlet weak var keyLabel: UILabel!
+    @IBOutlet weak var valueLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
